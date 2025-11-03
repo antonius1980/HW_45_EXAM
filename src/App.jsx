38cbd3e6d5fb08f5e53a0cda79526354f5992b3f
antonius1980 <antonius1980@gmail.com>
2025-11-03 17:29:14 +0200

    Fixing routing for local and GH

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-base: "/HW_45_EXAM/",
-})
+  // Use "/" in dev and "/HW_45_EXAM/" for production build (GH Pages)
+  base: command === 'build' ? '/HW_45_EXAM/' : '/',
+}))
