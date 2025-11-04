0d176ce412771bf17097744d0724909d87739075
antonius1980 <antonius1980@gmail.com>
2025-11-04 11:36:07 +0200

    Trying to configure vite.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   // Use "/" in dev and "/HW_45_EXAM/" for production build (GH Pages)
   base: command === 'build' ? '/HW_45_EXAM/' : '/',
+resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), //ex: "@/scss/..."
+    },
+  },
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/scss/_vars.scss" as *;
+          @use "@/scss/_mixins.scss" as *;
+        `
+      },
+    },
+  },
 }))
